272cc895472dc3d8ef00a7f0d947768624c5e537
Martyvonparty <martinaeriksson95@gmail.com>
2024-05-06 14:57:05 +0200

    ny projekt
     ny projekt

--- src/ngo_24/Projekt.java
+++ src/ngo_24/Projekt.java
@@ -2,18 +2,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
+package ngo_24;
 
+import oru.inf.InfDB;
+import oru.inf.InfException;
 /**
  *
  * @author marti
  */
 public class Projekt extends javax.swing.JFrame {
-
+    
+    private InfDB idb;
+    private String inloggadAnvandare;
     /**
      * Creates new form Projekt
      */
-    public Projekt() {
+    public Projekt(InfDB idb, String inloggadAnvandare) {
+        this.idb = idb;
+        this.inloggadAnvandare = inloggadAnvandare;
         initComponents();
+        lblInloggadAnvandare.setText(inloggadAnvandare);
     }
 
     /**
@@ -25,17 +33,27 @@ public class Projekt extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        lblInloggadAnvandare = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        lblInloggadAnvandare.setText("jLabel1");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(34, 34, 34)
+                .addComponent(lblInloggadAnvandare)
+                .addContainerGap(329, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(36, 36, 36)
+                .addComponent(lblInloggadAnvandare)
+                .addContainerGap(248, Short.MAX_VALUE))
         );
 
         pack();
@@ -71,11 +89,12 @@ public class Projekt extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Projekt().setVisible(true);
+                //new Projekt().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel lblInloggadAnvandare;
     // End of variables declaration//GEN-END:variables
 }
